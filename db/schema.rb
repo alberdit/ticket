e58b4508684204790303994353ac824434621607
alberdit <tomas.alberdi@gmail.com>
2022-03-12 21:19:17 +0000

    new file:   .README.md.swp 	modified:   Gemfile.lock 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_03_11_174643) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tickets", force: :cascade do |t|
     t.string "titulo"
     t.string "propietario"
